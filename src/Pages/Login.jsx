81694e0dde81fb54ada608b1d34fad4bb64e8c21
Thomas Abas <abasienyenethomas2@gmail.com>
2025-07-29 19:04:13 +0100

    added login and signup

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../services/firebase";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/dashboard");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+      <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+      <button type="submit">Login</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
+}
---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../services/firebase";
+import { useNavigate } from "react-router-dom";
+
+export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/dashboard");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSignup}>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+      <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+      <button type="submit">Sign Up</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
+}
